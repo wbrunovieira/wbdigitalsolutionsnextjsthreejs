0716a36c7ed87e3216e55ad9ab0f9590d20b2c14
wbrunovieira <wbrunovieira77@gmail.com>
2024-01-06 18:23:07 -0300

    added docker style in sidebar

--- src/components/SideSocial.tsx
+++ src/components/SideSocial.tsx
@@ -1,3 +1,5 @@
+
+import React, { useState } from 'react';
 import { PiInstagramLogo, PiFacebookLogo, PiWhatsappLogo, PiYoutubeLogo } from 'react-icons/pi';
   // nav data
   export const navData = [
@@ -12,30 +14,69 @@ import { PiInstagramLogo, PiFacebookLogo, PiWhatsappLogo, PiYoutubeLogo } from '
 
   
   const SideSocial = () => {
-    
+  
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+    const handleMouseEnter = (index: number) => {
+      setHoveredIndex(index);
+    };
+
+    const getIconStyle = (index: number) => {
+      let scale = 1;
+      let margin = '10px';
+  
+      if (hoveredIndex !== null) {
+        if (index === hoveredIndex) {
+          scale = 1.5;
+          margin = '20px';
+        } else if (Math.abs(index - hoveredIndex) === 1) {
+          scale = 1.1;
+          margin = '20px';
+        }
+      }
+  
+      return {
+        transform: `scale(${scale})`,
+        margin: `0 ${margin}`,
+        transition: 'transform 0.3s, margin 0.3s',
+      };
+    };
+
+    const handleMouseLeave = () => {
+      setHoveredIndex(null);
+    };
+
+    const getContainerStyle = () => {
+      return {
+        gap: hoveredIndex !== null ? '20px' : '10px', 
+        transition: 'gap 0.3s',
+      };
+    };
     
     return (
       <nav className="fixed bottom-0 top-0 z-50 mt-auto flex h-max w-full flex-col items-center gap-y-4 xl:right-[2%] xl:h-screen xl:w-16 xl:max-w-md xl:justify-center">
         {/* inner */}
         <div
           className="flex h-[80px] w-full items-center justify-between gap-y-10 bg-white/10 px-4 py-8 text-3xl backdrop-blur-sm md:px-40 xl:h-max xl:flex-col
-        xl:justify-center xl:rounded-full xl:px-0 xl:text-xl"
+        xl:justify-center xl:rounded-full xl:px-0 xl:text-xl dock" onMouseLeave={handleMouseLeave} style={getContainerStyle()} 
         >
           {navData.map((link, index) => {
             return (
-              <a
+              <a 
                 className={`${
                   link.href 
-                } hover:text-accent group relative flex items-center transition-all duration-300`}
+                } hover:text-accent group relative flex items-center transition-all duration-300 dock-item cursor-pointer`}
                 href={link.path}
                 target="_blank"
                 rel="noopener noreferrer"
                 key={index}
+                onMouseEnter={() => handleMouseEnter(index)}
+               
               >
                 {/* tooltip */}
                 <div className="absolute right-0 hidden pr-14 xl:group-hover:flex">
-                  <div className="text-primary relative flex items-center rounded-[3px] bg-white p-[6px]">
-                    <div className="text-[12px] font-semibold capitalize leading-none">
+                  <div className="text-primary relative flex items-center rounded-[3px] bg-white p-[12px]">
+                    <div className="text-[10px] font-semibold capitalize leading-none">
                       {link.name}
                     </div>
                     {/* triangle */}
@@ -43,7 +84,9 @@ import { PiInstagramLogo, PiFacebookLogo, PiWhatsappLogo, PiYoutubeLogo } from '
                   </div>
                 </div>
                 {/* icon */}
-                <div className="text-white">{link.icon}</div>
+                <div style={getIconStyle(index)}>
+              <div className="text-white">{link.icon}</div>
+            </div>
               </a>
             );
           })}
