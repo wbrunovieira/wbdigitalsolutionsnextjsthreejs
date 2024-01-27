0de9dc42cec2eb8e13f88f4550d20d3f2b6bf6b0
wbrunovieira <wbrunovieira77@gmail.com>
2024-01-27 17:41:47 -0300

    page sites, add threejs, two cubes and text

--- src/pages/websites/index.tsx
+++ src/pages/websites/index.tsx
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Html } from '@react-three/drei';
+import { Mesh } from 'three';
 
-const websites: React.FC = () => {
+const Websites: React.FC = () => {
   return (
     <>
-      <div className='text-lg text-white z-50 h-80 bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
-      <div className='text-lg text-white z-50 h80  bg-slate-300'>site</div>
+      <section className='bg-[#350545] h-screen'>
+        <Canvas
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 100,
+            position: [1, 1, 6],
+          }}
+        >
+          <Scene />
+        </Canvas>
+      </section>
     </>
   );
 };
 
-export default websites;
+const Scene = () => {
+  const cubeRef = useRef<Mesh>(null);
+
+  return (
+    <>
+      <OrbitControls />
+      <mesh position-x={1} ref={cubeRef}>
+        <boxGeometry />
+        <meshBasicMaterial color='orange' />
+        <Html
+          position={[-0.7, 0.5, 0.5]}
+          wrapperClass='text'
+          distanceFactor={5}
+          className='text-white'
+        >
+          WB Sites
+        </Html>
+      </mesh>
+      <mesh position-x={-1}>
+        <boxGeometry />
+        <meshBasicMaterial color='purple' />
+      </mesh>
+    </>
+  );
+};
+
+export default Websites;
