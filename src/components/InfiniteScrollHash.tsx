5b986498272acbb3cb47251476600022bc3c766e
bruno-minimac <wbrunovieira77@gmail.com>
2024-09-14 18:53:24 -0300

    multilanguage in Scroll Infinite component

--- src/components/InfiniteScrollHash.tsx
+++ src/components/InfiniteScrollHash.tsx
@@ -1,43 +1,55 @@
 "use client";
-// import { useLanguage } from "@/contexts/LanguageContext";
-// import { useTranslations } from "@/contexts/TranslationContext";
 
-// const { language, setLanguage } = useLanguage();
-
-// const currentMessages = useTranslations();
-
-// const tags = [
-//     currentMessages.websites,
-//     currentMessages.InteligenciaArtificial,
-// currentMessages.Automation,
-// currentMessages.digitalmarketingHash,
-// currentMessages.ecommerce,
-// currentMessages.socialmediaHash,
-// currentMessages.landingpage,
-// currentMessages.design,
-// currentMessages.digitalmarketingSeo,
-// ...(language === "pt-BR" ? currentMessages.wbsalao : '')
-// ];
-const tags = [
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-    "WebSites",
-];
+import React from "react";
+import { useTranslations } from "@/contexts/TranslationContext";
 
 const InfiniteScrollHash: React.FC = () => {
+    const currentMessages = useTranslations();
+
+    const tags = [
+        currentMessages.WebSitesKey,
+        currentMessages.CustomSystemsKey,
+        currentMessages.PaidAdsKey,
+        currentMessages.SocialMediaManagementKey,
+        currentMessages.OnlineStoreKey,
+        currentMessages.AutomationKey,
+        currentMessages.ArtificialIntelligenceKey,
+        currentMessages.DataScienceKey,
+        currentMessages.DigitalMarketingKey,
+        currentMessages.GraphicDesignKey,
+        currentMessages.SEOOptimizationKey,
+        currentMessages.ResponsiveDesignKey,
+        currentMessages.UIUXDesignKey,
+        currentMessages.EcommerceDevelopmentKey,
+        currentMessages.AppDevelopmentKey,
+        currentMessages.ConversionOptimizationKey,
+        currentMessages.BrandStrategyKey,
+        currentMessages.ContentCreationKey,
+        currentMessages.EmailMarketingKey,
+        currentMessages.PerformanceAnalyticsKey,
+        currentMessages.SocialMediaAdsKey,
+        currentMessages.LeadGenerationKey,
+        currentMessages.MarketingAutomationKey,
+        currentMessages.InfluencerMarketingKey,
+        currentMessages.CampaignManagementKey,
+        currentMessages.LandingPagesKey,
+        currentMessages.ABTestingKey,
+        currentMessages.DataAnalyticsKey,
+        currentMessages.ChatbotsKey,
+        currentMessages.MachineLearningKey,
+        currentMessages.CloudSolutionsKey,
+        currentMessages.CRMIntegrationKey,
+        currentMessages.PaymentGatewayIntegrationKey,
+        currentMessages.ProductPhotographyKey,
+        currentMessages.VideoEditingKey,
+        currentMessages.CopywritingKey,
+        currentMessages.LogoDesignKey,
+        currentMessages.ConversionTrackingKey,
+        currentMessages.MarketResearchKey,
+        currentMessages.VirtualRealityKey,
+        currentMessages.AugmentedRealityKey,
+    ];
+
     return (
         <section className="w-screen p-10 bg-[#350545]">
             <div className="tag-list p-4">
@@ -56,3 +68,69 @@ const InfiniteScrollHash: React.FC = () => {
 };
 
 export default InfiniteScrollHash;
+
+// "use client";
+
+// const tags = [
+//     "WebSites",
+//     "CustomSystems",
+//     "PaidAds",
+//     "SocialMediaManagement",
+//     "OnlineStore",
+//     "Automation",
+//     "ArtificialIntelligence",
+//     "DataScience",
+//     "DigitalMarketing",
+//     "GraphicDesign",
+//     "SEOOptimization",
+//     "ResponsiveDesign",
+//     "UIUXDesign",
+//     "EcommerceDevelopment",
+//     "AppDevelopment",
+//     "ConversionOptimization",
+//     "BrandStrategy",
+//     "ContentCreation",
+//     "EmailMarketing",
+//     "PerformanceAnalytics",
+//     "SocialMediaAds",
+//     "LeadGeneration",
+//     "MarketingAutomation",
+//     "InfluencerMarketing",
+//     "CampaignManagement",
+//     "LandingPages",
+//     "ABTesting",
+//     "DataAnalytics",
+//     "Chatbots",
+//     "MachineLearning",
+//     "CloudSolutions",
+//     "CRMIntegration",
+//     "PaymentGatewayIntegration",
+//     "ProductPhotography",
+//     "VideoEditing",
+//     "Copywriting",
+//     "LogoDesign",
+//     "ConversionTracking",
+//     "MarketResearch",
+//     "VirtualReality",
+//     "AugmentedReality",
+// ];
+
+// const InfiniteScrollHash: React.FC = () => {
+//     return (
+//         <section className="w-screen p-10 bg-[#350545]">
+//             <div className="tag-list p-4">
+//                 <div className="inner">
+//                     {tags.map((tag, index) => (
+//                         <div className="tag" key={index}>
+//                             <span>#</span>
+//                             {tag}
+//                         </div>
+//                     ))}
+//                 </div>
+//                 <div className="fade"></div>
+//             </div>
+//         </section>
+//     );
+// };
+
+// export default InfiniteScrollHash;
