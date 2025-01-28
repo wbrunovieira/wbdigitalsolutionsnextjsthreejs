34a51075af0b38117905fc2b146c64311a37828f
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2025-01-28 18:51:15 -0300

    added LLMs section in ai page

--- src/pages/ai/index.tsx
+++ src/pages/ai/index.tsx
@@ -1,17 +1,25 @@
 
 import { AIHeader } from '@/components/AIHeader';
+
 import AnimatedBackgroundAIComponent from '@/components/AnimatedBackgoundAIComponent';
-import React from 'react';
+import dynamic from 'next/dynamic';
+
+
+const LLMSection = dynamic(() => import('@/components/AILLMSection'), { ssr: false });
 
 const ai: React.FC = () => {
   return <main>
-      <div className="relative w-full h-screen overflow-hidden mt-32">
-        <div className="absolute inset-0 z-0">
+      <div className="relative w-full mt-32 ">
+        <div className="fixed inset-0 z-0">
 
               <AnimatedBackgroundAIComponent />
         </div>
         <div className="relative z-10">
           <AIHeader scrollIndicatorHidden={true} />
+        </div>
+         <div className="relative z-10">
+
+        <LLMSection  />
         </div>
     </div>
   </main>
