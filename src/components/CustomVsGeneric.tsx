5cbf81f53d7be5634c60aae348df210f45c7814d
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2025-02-02 08:38:40 -0300

    modified files in CustomVsGeneric.tsx and index.tsx

--- src/components/CustomVsGeneric.tsx
+++ src/components/CustomVsGeneric.tsx
@@ -18,7 +18,7 @@ const CustomVsGeneric: React.FC = () => {
       const ScrollTrigger = require("gsap/ScrollTrigger").default;
       gsap.registerPlugin(ScrollTrigger);
 
-      // Title animation
+
       gsap.from(titleRef.current, {
         opacity: 0,
         y: -50,
@@ -30,7 +30,7 @@ const CustomVsGeneric: React.FC = () => {
         },
       });
 
-      // Subtitle animation
+
       gsap.from(subtitleRef.current, {
         opacity: 0,
         y: 30,
@@ -43,7 +43,7 @@ const CustomVsGeneric: React.FC = () => {
         },
       });
 
-      // Comparison section animation
+
       gsap.from(comparisonRef.current, {
         opacity: 0,
         scale: 0.9,
@@ -51,86 +51,91 @@ const CustomVsGeneric: React.FC = () => {
         ease: "power3.out",
         scrollTrigger: {
           trigger: comparisonRef.current,
-          start: "top 75%",
+          start: "top 90%",
         },
       });
     }
   }, []);
 
   return (
-    <section ref={sectionRef} className="bg-primary relative flex flex-col items-center px-6 py-12 lg:px-20 lg:py-24 mt-32">
-       <div className="relative w-full max-w-3xl mb-12 text-center">
-        <FaStar className="text-yellow-400 text-4xl mx-auto mb-4" />
-        <h2
-          ref={titleRef}
-          className="text-3xl lg:text-5xl font-bold text-center text-white mb-6"
-        >
-          {currentMessages.experienceTitleWebsite}
-        </h2>
-        <span className="text-3xl lg:text-5xl text-yellowcustom font-bold">   {currentMessages.experienceTitleWebsite2}</span>
-        <div className="mt-4 w-96 h-1 bg-gradient-to-r from-yellow-400 to-transparent mx-auto mb-6 pl-2"></div>
-        <p
-          ref={subtitleRef}
-          className="flex text-lg lg:text-xl text-left text-white max-w-3xl mx-auto"
-        >
-          <FaCopy className="text-yellow-400 text-4xl mr-4 p-2" />
-          {currentMessages.experienceSubtitle}
-        </p>
-        <p
-          ref={subtitleRef}
-          className="flex text-lg lg:text-xl text-left text-white max-w-3xl mx-auto mt-2"
-        >
-           <FaPaintBrush className=" text-yellow-400 text-4xl mr-4 p-2" />
-          {currentMessages.experienceSubtitle1_1}
-        </p>
-        <p ref={subtitleRef}  className="flex text-lg lg:text-xl text-left text-white max-w-3xl mx-auto mt-2">
-          <FaChartLine className="text-yellow-400 text-4xl mr-4 p-2" />
-          {currentMessages.experienceSubtitle1_2} s 
-        </p> 
-        <p ref={subtitleRef}  className="flex text-lg lg:text-xl text-left text-white max-w-3xl mx-auto mt-2">
-         <FaRocket className="text-yellow-400 text-4xl mr-4 p-2" />
-          {currentMessages.experienceSubtitle1_3}
-        </p>
-        <p ref={subtitleRef}  className="flex text-2xl lg:text-3xl text-left text-yellowcustom max-w-3xl mx-auto mt-4 font-light p-2">
-         
-          {currentMessages.experienceSubtitle1_4}
-        </p>
-        <p ref={subtitleRef} className="bg-white w-1/2 mt-4 px-2 py-2 rounded text-lg lg:text-xl text-center text-primary max-w-3xl mx-auto p-2">
-          {currentMessages.experienceSubtitle2}
-        </p>
-
-      </div>
-
-      <div
-        ref={comparisonRef}
-        className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full max-w-5xl relative"
+    <section
+        ref={sectionRef}
+        className="bg-primary relative flex flex-col items-center px-6 sm:px-8 md:px-12 lg:px-20 py-12 lg:py-24 mt-32"
       >
-        {/* Modern Divider */}
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="w-full lg:w-1 lg:h-full bg-gradient-to-b from-gray-300 to-transparent lg:mx-4"></div>
-        </div>
+       <div className="container mx-auto px-4 sm:px-8">
+
+          <div className="relative w-full max-w-3xl mb-12 text-center mx-auto">
+            <FaStar className="text-yellow-400 text-4xl mx-auto mb-4" />
+            <h2
+              ref={titleRef}
+              className="text-3xl lg:text-5xl font-bold text-center text-white mb-6"
+            >
+              {currentMessages.experienceTitleWebsite}
+            </h2>
+            <span className="text-3xl lg:text-5xl text-yellowcustom font-bold mb-6">   {currentMessages.experienceTitleWebsite2}</span>
+
+
+            <p
+              ref={subtitleRef}
+              className="flex text-lg lg:text-xl text-left text-white max-w-3xl mx-auto mt-4 mb-4"
+            >
+              <FaCopy className="text-yellow-400 text-4xl mr-4 p-2" />
+              {currentMessages.experienceSubtitle}
+            </p>
+            <p
+              ref={subtitleRef}
+              className="flex text-lg lg:text-xl text-left text-white max-w-3xl mx-auto mt-2"
+            >
+              <FaPaintBrush className=" text-yellow-400 text-4xl mr-4 p-2" />
+              {currentMessages.experienceSubtitle1_1}
+            </p>
+            <p ref={subtitleRef}  className="flex text-lg lg:text-xl text-left text-white max-w-3xl mx-auto mt-2">
+              <FaChartLine className="text-yellow-400 text-4xl mr-4 p-2" />
+              {currentMessages.experienceSubtitle1_2} s 
+            </p> 
+            <p ref={subtitleRef}  className="flex text-lg lg:text-xl text-left text-white max-w-3xl mx-auto mt-2 no-whitespace">
+            <FaRocket className="text-yellow-400 text-4xl mr-4 p-2" />
+              {currentMessages.experienceSubtitle1_3}
+            </p>
+            <p ref={subtitleRef}  className="flex text-2xl lg:text-3xl text-left text-yellowcustom max-w-3xl mx-auto mt-4 font-light p-2">
+            
+              {currentMessages.experienceSubtitle1_4}
+            </p>
+            <p ref={subtitleRef} className="bg-white w-1/2 mt-4 px-2 py-2 rounded text-lg lg:text-xl text-center text-primary max-w-3xl mx-auto p-2">
+              {currentMessages.experienceSubtitle2}
+            </p>
+
+          </div>
+
+          <div
+            ref={comparisonRef}
+            className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full max-w-5xl relative"
+          >
 
 
 
-        {/* Custom Example */}
-        <div className="flex flex-col items-center text-left bg-custom-purple shadow-md p-6 rounded-lg relative">
-          <FaCopy className="text-yellowcustom text-4xl mb-4" />
-          <h3 className="text-xl font-semibold text-white mb-4">
-            {currentMessages.genericSolution}
-          </h3>
-          <p className="text-white mb-6 " dangerouslySetInnerHTML={{ __html: currentMessages.genericDescription }}></p>
-          <div className="absolute top-0 left-0 w-1 h-full bg-white"></div>
-        </div>
 
-        <div className="flex flex-col items-center text-left bg-custom-purple shadow-md p-6 rounded-lg relative">
-          <FaRocket className="text-yellowcustom text-4xl mb-4" />
-          <h3 className="text-xl font-semibold text-white mb-4">
-            {currentMessages.customSolution}
-          </h3>
-          <p className="text-white mb-6" dangerouslySetInnerHTML={{ __html: currentMessages.customDescription }}></p>
-          <div className="absolute top-0 left-0 w-1 h-full bg-gradient-to-b from-custom-purple to-yellowcustom"></div>
-        </div>
-      </div>
+            {/* Custom Example */}
+            <div className="flex flex-col items-center text-left bg-custom-purple shadow-md p-6 rounded-lg relative">
+              <FaCopy className="text-yellowcustom text-4xl mb-4" />
+              <h3 className="text-xl font-semibold text-white mb-4">
+                {currentMessages.genericSolution}
+              </h3>
+              <p className="text-white mb-6 " dangerouslySetInnerHTML={{ __html: currentMessages.genericDescription }}></p>
+              <div className="absolute top-0 left-0 w-1 h-full bg-white"></div>
+            </div>
+
+            <div className="flex flex-col items-center text-left bg-custom-purple shadow-md p-6 rounded-lg relative">
+              <FaRocket className="text-yellowcustom text-4xl mb-4" />
+              <h3 className="text-xl font-semibold text-white mb-4">
+                {currentMessages.customSolution}
+              </h3>
+              <p className="text-white mb-6" dangerouslySetInnerHTML={{ __html: currentMessages.customDescription }}></p>
+              <div className="absolute top-0 left-0 w-1 h-full bg-gradient-to-b from-custom-purple to-yellowcustom"></div>
+            </div>
+          </div>
+
+    </div>
     </section>
   );
 };
