a68858c8014d66b753c5f153e98288543e66b1de
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2025-03-11 12:02:15 -0300

    form in footer is working

--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -1,59 +1,56 @@
 "use client";
 import React, { useState } from "react";
-import ButtonStandard from "./ButtonStandard";
 import { useTranslations } from "@/contexts/TranslationContext";
 
-const EmailInput = () => {
-    const currentMessages = useTranslations();
-    const [email, setEmail] = useState("");
-    const [isEmailValid, setIsEmailValid] = useState(true);
+interface EmailInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setEmail(value);
-        setIsEmailValid(isValidEmail(value));
-    };
+const EmailInput: React.FC<EmailInputProps> = ({ value, onChange }) => {
+  const currentMessages = useTranslations();
+  const [isEmailValid, setIsEmailValid] = useState(true);
 
-    const handleBlur = () => {
-        setIsEmailValid(isValidEmail(email));
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const emailValue = e.target.value;
+    onChange(emailValue);
+    setIsEmailValid(isValidEmail(emailValue));
+  };
 
-    const isValidEmail = (email: string) => {
-        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-        return emailRegex.test(email);
-    };
+  const handleBlur = () => {
+    setIsEmailValid(isValidEmail(value));
+  };
 
-    return (
-        <div className="w-full flex flex-end flex-col relative">
-            <div className="flex items-center gap-2">
-                <input
-                    type="email"
-                    name="email"
-                    placeholder={currentMessages.enterEmail}
-                    required
-                    value={email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={`flex-grow lg:w-[500px] xl:w-[400px] p-3 rounded-lg border ${
-                        isEmailValid ? "border-gray-300" : "border-red-500"
-                    } text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 ${
-                        isEmailValid
-                            ? "focus:ring-yellowcustom"
-                            : "focus:ring-red-500"
-                    } focus:border-transparent transition-colors duration-200 ease-in-out shadow-sm h-12`}
-                />
-                
-            </div>
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return emailRegex.test(email);
+  };
 
-       
-            {!isEmailValid && (
-                <p className="absolute left-0 top-[calc(100%+4px)] text-red-500 text-sm">
-                    {currentMessages.validEmail}
-                </p>
-            )}
-             
-        </div>
-    );
+  return (
+    <div className="w-full flex flex-col relative">
+      <div className="flex items-center gap-2">
+        <input
+          type="email"
+          name="email"
+          placeholder={currentMessages.enterEmail}
+          required
+          value={value}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={`flex-grow lg:w-[500px] xl:w-[400px] p-3 rounded-lg border ${
+            isEmailValid ? "border-gray-300" : "border-red-500"
+          } text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 ${
+            isEmailValid ? "focus:ring-yellowcustom" : "focus:ring-red-500"
+          } focus:border-transparent transition-colors duration-200 ease-in-out shadow-sm h-12`}
+        />
+      </div>
+      {!isEmailValid && (
+        <p className="absolute left-0 top-[calc(100%+4px)] text-red-500 text-sm">
+          {currentMessages.validEmail}
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default EmailInput;
